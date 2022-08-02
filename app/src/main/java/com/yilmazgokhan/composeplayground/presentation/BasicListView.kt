28e9fdf0c3ed68732655d09cb8b5d03ea9e89d6f
yilmazgokhan <yilmaz_gokhan@yahoo.com>
2022-08-03 01:40:11 +0300

    Integration of coil

--- app/src/main/java/com/yilmazgokhan/composeplayground/presentation/BasicListView.kt
+++ app/src/main/java/com/yilmazgokhan/composeplayground/presentation/BasicListView.kt
@@ -3,16 +3,24 @@ package com.yilmazgokhan.composeplayground.presentation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import android.R
 
 @Composable
 fun BasicListView() {
@@ -22,12 +30,14 @@ fun BasicListView() {
         ListItem(
             "Gokhan",
             "logicerror",
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            "https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?w=2000"
         ),
         ListItem(
             "James",
             "temp_username",
-            "Sed at aliquam libero, viverra."
+            "Sed at aliquam libero, viverra.",
+            "https://www.rollingstone.com/wp-content/uploads/2021/10/B25_37397_RC2_rgbC.jpg?resize=1800,1200&w=450"
         )
     )
     //endregion
@@ -54,6 +64,23 @@ fun RenderItem(item: ListItem) {
         elevation = 4.dp
     ) {
         Column(modifier = Modifier.padding(all = 10.dp)) {
+            AsyncImage(
+                model = item.imgUrl,
+                contentDescription = "",
+                modifier = Modifier.size(128.dp)
+            )
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(item.imgUrl)
+                    .crossfade(true)
+                    .build(),
+                //placeholder = painterResource(R.drawable.ic_lock_idle_alarm),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(CircleShape)
+            )
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     text = item.name,
@@ -63,7 +90,7 @@ fun RenderItem(item: ListItem) {
                 Text(
                     text = " - (${item.username})",
                     color = Color.Gray,
-                    fontSize = 14.sp
+                    fontSize = 14.sp,
                 )
             }
             Spacer(modifier = Modifier.height(2.dp))
@@ -79,5 +106,6 @@ fun RenderItem(item: ListItem) {
 data class ListItem(
     val name: String,
     val username: String,
-    val summary: String
+    val summary: String,
+    val imgUrl: String
 )
